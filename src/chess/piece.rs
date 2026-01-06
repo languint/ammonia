eed9279912ad510ba2968454fd4d6fdcbe1954f3
languint <longuint@proton.me>
2026-01-06 14:17:47 -0600

    feat: add Piece::ALL associated constant

--- src/chess/piece.rs
+++ src/chess/piece.rs
@@ -1,3 +1,5 @@
+use crate::chess::{nrof::NrOf, piece};
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub struct Piece(u8);
 impl Piece {
@@ -7,4 +9,13 @@ impl Piece {
     pub const ROOK: Piece = Piece(3);
     pub const QUEEN: Piece = Piece(4);
     pub const KING: Piece = Piece(5);
+
+    pub const ALL: [Piece; NrOf::PIECES] = [
+        Piece::PAWN,
+        Piece::KNIGHT,
+        Piece::BISHOP,
+        Piece::ROOK,
+        Piece::QUEEN,
+        Piece::KING,
+    ];
 }
